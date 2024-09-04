876a29658e89cd3a87e6b95f95d9d31f5b840ec3
Chloe-JieningYang <yjn_for_git@163.com>
2024-09-04 22:13:40 +0800

    Add vehicles.json file

--- convert_xy_to_gis.py
+++ convert_xy_to_gis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a44a66f5df4d9756963b6e153e072d92d9badd2ffb787f4ffaa95e81781a55ab
-size 3879
+oid sha256:1bc6a807ad55dbce4bb2abea84291c8c2ed176b3f44a20df322a1feb06e68457
+size 3837
--- safety_evaluation.py
+++ safety_evaluation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efa84337ddabdf22529f653b50cc231e4c962026489de6ba2603efa125f1d299
-size 2658
+oid sha256:6635c8749b7d51b94ffacf72f502cda5a31d21fed64a3fc15a38ff9fc525e186
+size 2697
