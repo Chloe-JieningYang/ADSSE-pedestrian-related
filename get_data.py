e4af60167c19c0ab8a922fd7c659a8c6340404d3
Chloe-JieningYang <yjn_for_git@163.com>
2024-10-02 22:50:51 +0800

    Changed get_data to adapt to the change of the REST API. Added filter for OBJECT to ego_vehicle data acquirement.

--- get_data.py
+++ get_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2a7aee49a5e866543edc3de55c59e470bf715382229359ff0fcc4fdc29264d9
-size 3624
+oid sha256:b351d25c81c1b0b304aa6335b413b2b68a07663f4b496c5586b7c1085def54d8
+size 3818
